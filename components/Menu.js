d50c115217088294d020bcdb75cf91d6f81c40e6
Renan Pereira Rocha <rntecnologia.inf@gmail.com>
2022-05-13 17:52:18 -0300

    finalização do menu, mudança para react-bootstrap

--- components/Menu.js
+++ components/Menu.js
@@ -1,55 +1,57 @@
 import React, { useState } from 'react';
-import Image from 'next/image'
-import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink,Button} from 'reactstrap';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {library} from '@fortawesome/fontawesome-svg-core';
-import {fas} from '@fortawesome/free-solid-svg-icons';
-library.add(fas);
+import Image from 'next/image';
+import {Container, Navbar, Nav, Button, Offcanvas, Dropdown, ButtonGroup} from 'react-bootstrap';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import profilePic from '../public/logo.webp';
+import styles from '../styles/Home.module.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Menu = (props) => {
+library.add(fas)
 
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
-return (
-    <div>
-<Navbar color="light" container="sm" expand="md" sticky="top" light>
-    <NavbarBrand href="rntecinfo.com.br">
-    <Image
-        src="/logo.webp"
-        alt="RN Tecnologia e Informática"
-        width={280}
-        height={45}/>
-    </NavbarBrand>
-    <NavbarToggler onClick={toggle} />
-    <Collapse isOpen={!isOpen} navbar>
-<Nav className='me-auto' navbar>
-        <NavItem>
-<NavLink href="/">
-<h6><FontAwesomeIcon icon='house' /> INÍCIO</h6>
-</NavLink>
-</NavItem>
-<NavItem>
-<NavLink href="/">
-<h6><FontAwesomeIcon icon='users' /> SERVIÇOS</h6>
-</NavLink>
-</NavItem>
-<NavItem>
-<NavLink href="/projetos">
-<h6><FontAwesomeIcon icon='suitcase' /> PROJETOS</h6>
-</NavLink>
-</NavItem>
-<NavItem>
-<NavLink href="/sobre">
-<h6><FontAwesomeIcon icon='building' />  SOBRE</h6>
-</NavLink>
-</NavItem>
-</Nav>
-<Button href="https://m.me/109368571498533"color="primary">
-<h6><FontAwesomeIcon icon='comment' /> CONTATO </h6>
-</Button>
-</Collapse>
-</Navbar>
-</div>
+const Menu = () => {  
+return ( 
+<>
+  {['xl'].map((expand) => (
+    <Navbar key={expand} bg="light" expand={expand} className="mb-3" sticky="top">
+      <Container >
+        <Navbar.Brand href="/"><Image src={profilePic} alt="RN Tecnologia e Informática" width={280} height={45} priority/></Navbar.Brand>
+        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+        <Navbar.Offcanvas
+          id={`offcanvasNavbar-expand-${expand}`}
+          aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+            <Image src={profilePic} alt="RN Tecnologia e Informática" width={280} height={45} priority/>
+            </Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="me-auto" >
+        <Nav.Link className={styles.smenu} href="/" ><FontAwesomeIcon icon='house' /> INÍCIO</Nav.Link>
+        <Nav.Link className={styles.smenu} href="/servicos"><FontAwesomeIcon icon='users' /> SERVIÇOS</Nav.Link>
+        <Nav.Link className={styles.smenu} href="/projetos"><FontAwesomeIcon icon='suitcase' /> PROJETOS</Nav.Link>
+        <Nav.Link className={styles.smenu} href="/sobre"><FontAwesomeIcon icon='building' />  SOBRE</Nav.Link>
+        <Dropdown as={ButtonGroup}>
+  <Button className={styles.botao}><FontAwesomeIcon  icon='ranking-star' /> CRM RN</Button>
+  <Dropdown.Toggle split  id="dropdown-split-basic" />
+  <Dropdown.Menu>
+    <Dropdown.Item className={styles.smenu} href="/crm-recursos"><FontAwesomeIcon  icon='arrows-spin' /> Recursos</Dropdown.Item>
+    <Dropdown.Item className={styles.smenu} href="/crm-planos"><FontAwesomeIcon  icon='credit-card' /> Planos</Dropdown.Item>
+    <Dropdown.Item className={styles.smenu} href="crm.rntecinfo.com.br" eventKey="4"><FontAwesomeIcon icon='address-card' /> Acesse à Área do Cliente</Dropdown.Item>
+  </Dropdown.Menu>
+</Dropdown>
+            </Nav>
+        <Button eventKey={2} className={styles.botao} href="https://m.me/109368571498533"><FontAwesomeIcon fade icon='comment' size="lg"  pull="left" /> CONTATO</Button>{' '}
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  ))}
+</>
   );
 };
 export default Menu;
--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,51 @@
 import Menu from "../components/Menu";
 import Head from "next/head";
 import Image from 'next/image'
-import profilePic from '../public/tecnologia.png'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Container, Row, Col, Button} from 'reactstrap';
+import profilePic from '../public/informatica-e-tecnologia.png'
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import React from 'react';
 
 function Inicio() {
-return (
+  return (
     <div>
-<Head><title>Renan Tecnologia e Informática - Programador e Engenheiro</title>
+      <Head><title>Renan Tecnologia e Informática - Programador e Engenheiro</title>
         <meta name="description" content="Programador e prestador de serviço de informática em São Gonçalo, Niterói, Itaboraí e Rio de Janeiro." />
         <meta name="keywords" content="serviço de informática, serviço de informática em São Gonçalo, programador, serviço de informática em Niterói, serviço de informática em Itaboraí, serviço de informática mo rio de janeiro." />
         <meta name="author" content="Renan Pereira Rocha" />
         <meta name="email" content="contato@rntecinfo.com.br" />
         <meta name="website" content="https://www.rntecinfo.com.br" />
-</Head>
-<Menu/>
-<Container >
-<Row xs="2">
-<Col className="bg-light border">
-<>
-<Image src={profilePic} alt="Picture of the author"/>
-</>
-    </Col>
-    <Col className="bg-light border">
-      <h1>Olá, Seja Bem Vindo(a) Navegue e Conheça Meu Portfólio</h1>
-      <p>Prestador de serviços no ramo da Informática e da tecnologia, experientes na área com mão de obra especializada e comprometida com o serviço, preparado para atender todos os tipos de clientes, oferecendo assim, uma solução inteligente, de qualidade que caiba em seu bouço.</p>
-    </Col>
-  </Row>
-  </Container>
+      </Head>
+      <Menu />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Container >
+        <Row xs="2">
+          <Col>
+            <>
+              <Image src={profilePic} alt="Picture of the author" />
+            </>
+          </Col>
+          <Col >
+            <h1>Olá, Seja Bem Vindo(a) Navegue e Conheça Meu Portfólio</h1>
+            <p>Prestador de serviços no ramo da Informática e da tecnologia, experientes na área com mão de obra especializada e comprometida com o serviço, preparado para atender todos os tipos de clientes, oferecendo assim, uma solução inteligente, de qualidade que caiba em seu bouço.</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
 
-);
+  );
 }
 
 export default Inicio
